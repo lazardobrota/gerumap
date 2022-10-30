f1c4a551ddd344a93de2de475183aa9ace411bf0
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-10-30 18:20:09 +0100

    pgn != png

--- src/raf/dsw/gerumap/gui/swing/controller/EditAction.java
+++ src/raf/dsw/gerumap/gui/swing/controller/EditAction.java
@@ -9,7 +9,7 @@ public class EditAction extends AbstractGerumapAction{
     public EditAction(){
 
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_E,ActionEvent.CTRL_MASK));
-        putValue(SMALL_ICON,loadIcon("images/edit.pgn"));
+        putValue(SMALL_ICON,loadIcon("images/edit.png"));
         putValue(NAME,"Edit");
         putValue(SHORT_DESCRIPTION,"Izmeni");
     }
