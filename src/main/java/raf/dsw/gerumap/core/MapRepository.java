405b0b36c67cbd5f9e31e3e6944eea0a62abb7ef
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-02 13:29:31 +0100

    Dodat ProjectExplorer objekat i metoda addChild

--- src/main/java/raf/dsw/gerumap/core/MapRepository.java
+++ src/main/java/raf/dsw/gerumap/core/MapRepository.java
@@ -1,4 +1,12 @@
 package raf.dsw.gerumap.core;
 
+import raf.dsw.gerumap.mapRepository.composite.MapNode;
+import raf.dsw.gerumap.mapRepository.composite.MapNodeComposite;
+import raf.dsw.gerumap.mapRepository.implementation.ProjectExplorer;
+
 public interface MapRepository {
+
+    ProjectExplorer getProjectExplorer();
+
+    boolean addChild(MapNodeComposite parent, MapNode child);
 }
