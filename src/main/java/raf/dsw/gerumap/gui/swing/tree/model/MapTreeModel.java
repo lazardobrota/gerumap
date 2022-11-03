5b85989e46341c79556d72f012044b48fbcba75b
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-03 18:18:01 +0100

    Dodata nova klasa

---
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/model/MapTreeModel.java
@@ -0,0 +1,11 @@
+package raf.dsw.gerumap.gui.swing.tree.model;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+public class MapTreeModel extends DefaultTreeModel {
+
+    public MapTreeModel(TreeNode root) {
+        super(root);
+    }
+}
