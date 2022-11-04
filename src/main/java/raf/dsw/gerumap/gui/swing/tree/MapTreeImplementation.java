f9f5c3c303ce43d4346a3063fc5d6b48568080d0
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-04 15:03:46 +0100

    Dodat uslov da ne mogu cvorovi da imaju ista imena kada se prvi put prave

--- src/main/java/raf/dsw/gerumap/gui/swing/tree/MapTreeImplementation.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/MapTreeImplementation.java
@@ -27,21 +27,46 @@ public class MapTreeImplementation implements MapTree{
     }
 
     private MapNode createChild(MapNode parent){
+
+        MapNodeComposite p = null;
+        MapNode child;
+
+        //Da bi mogli da koristimo addChild() metodu koja sama proverava ako ima decu sa istim imenima
+        if (parent instanceof MapNodeComposite)
+            p = (MapNodeComposite) parent;
+
         if(parent instanceof ProjectExplorer){
-            return new Project("Project", parent);
+            child = new Project("Project" + p.getChildren().size(), parent);
+            if (!p.getChildren().contains(child)) {
+                p.addChild(child);
+                return child;
+            }
         }
         else if (parent instanceof Project) {
-            return new MindMap("MindMap", parent, false);
+            child = new MindMap("MindMap" + p.getChildren().size(), parent, false);
+            if (!p.getChildren().contains(child)) {
+                p.addChild(child);
+                return child;
+            }
         }
-        else {
-            return new Element("Element", parent);
+        else if (parent instanceof MindMap) {
+            child = new Element("Element" + p.getChildren().size(), parent);
+            if (!p.getChildren().contains(child)) {
+                p.addChild(child);
+                return child;
+            }
         }
+
+        return null;
     }
 
     @Override
     public void addChild(MapTreeItem parent) {
         if(parent.getMapNode() instanceof MapNodeComposite){
             MapNode child = this.createChild(parent.getMapNode());
+            //child je null kada vec postoji MapNode sa tim imenom kod tog parenta
+            if (child == null)
+                return;
             //proverava jel to project explorer npr i dodaje dete
             parent.add(new MapTreeItem(child));//ovde dodaje novu decu i to vidimo
             mapTreeView.expandPath(mapTreeView.getSelectionPath());//i osvezavamo
