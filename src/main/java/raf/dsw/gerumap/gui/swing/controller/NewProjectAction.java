17ca43537b54fad97b2f1ed94264a2ac9834e859
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-04 18:22:11 +0100

    ime slike promenjeno

--- src/main/java/raf/dsw/gerumap/gui/swing/controller/NewProjectAction.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/controller/NewProjectAction.java
@@ -6,13 +6,12 @@ import raf.dsw.gerumap.gui.swing.view.MainFrame;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.Random;
 
 public class NewProjectAction extends AbstractGerumapAction{
 
     public NewProjectAction() {
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));//ACCELERATOR_KEY - zadajemo precicu
-        putValue(SMALL_ICON, loadIcon("/images/pluss.png"));//jos uvek nemam nista u images paketu ali dodaje ikonicu za New Project, verovatno u MenuBar ako tamo napravimo instancu klase
+        putValue(SMALL_ICON, loadIcon("/images/plus.png"));//jos uvek nemam nista u images paketu ali dodaje ikonicu za New Project, verovatno u MenuBar ako tamo napravimo instancu klase
         putValue(NAME, "New Project");//Zvace se New Project
         putValue(SHORT_DESCRIPTION, "Napravi novi projekat");//Ono sto ce pisati ako drzimo mis duze na ikonicu kao objasnjenje sta radi
     }
