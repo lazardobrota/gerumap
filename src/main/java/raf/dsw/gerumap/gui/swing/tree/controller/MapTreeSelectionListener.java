6d6f4d4acc8f2d928de43b923eaf337ca5a8207f
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-04 14:32:53 +0100

    dodati komentari za lakse objasnjenje i MapTreeSelectionListener path ispisuje imena cvorova

--- src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeSelectionListener.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/controller/MapTreeSelectionListener.java
@@ -5,13 +5,14 @@ import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreePath;
+import java.util.Arrays;
 
 public class MapTreeSelectionListener implements TreeSelectionListener {
     @Override
     public void valueChanged(TreeSelectionEvent e) {
-        TreePath path = e.getPath();
-        MapTreeItem itemSelected = (MapTreeItem) path.getLastPathComponent();
+        TreePath path = e.getPath();//putanja do selektovanog cvora
+        MapTreeItem itemSelected = (MapTreeItem) path.getLastPathComponent(); // uzima cvor koji je selektovan
         System.out.println("Selektovan cvor: " + itemSelected.getMapNode().getIme());
-        System.out.println("getPath: " + path.getPath());
+        System.out.println("getPath: " + Arrays.toString(path.getPath()));
     }
 }
--- src/main/java/raf/dsw/gerumap/gui/swing/tree/view/MapTreeView.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/tree/view/MapTreeView.java
@@ -6,14 +6,17 @@ import raf.dsw.gerumap.gui.swing.tree.controller.MapTreeSelectionListener;
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeModel;
 
+/**
+ * Ovde se sve klase medjusobno povezuju i prikazuju na aplikaciju
+ */
 public class MapTreeView extends JTree {
 
     public MapTreeView(DefaultTreeModel defaultTreeModel) {
-        this.setModel(defaultTreeModel);//ovo je cvor i modeli koji se prikazuju
-        MapTreeCellRenderer mapTreeCellRenderer = new MapTreeCellRenderer();
-        this.addTreeSelectionListener(new MapTreeSelectionListener());
-        this.setCellEditor(new MapTreeCellEditor(this, mapTreeCellRenderer));
+        this.setModel(defaultTreeModel);//gleda koje je ime cvora da ga postavi, toString() metoda
+        MapTreeCellRenderer mapTreeCellRenderer = new MapTreeCellRenderer();//za slike
+        this.addTreeSelectionListener(new MapTreeSelectionListener());//Posmatra da li ima promene na tree
+        this.setCellEditor(new MapTreeCellEditor(this, mapTreeCellRenderer));//Klasa koja ce da edituje cvorove(menjanje imena, ...)
         this.setCellRenderer(mapTreeCellRenderer);
-        this.setEditable(true);
+        this.setEditable(true);//tree nije readOnly nego moze da se edituje(menjaju imena cvorova, dodaju novi cvorovi, ...)
     }
 }
