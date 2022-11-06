8167c5f937182e3aeae418abc25dcd2bc6e45476
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-06 12:34:42 +0100

    dodat numberingChild atribut

--- src/main/java/raf/dsw/gerumap/mapRepository/composite/MapNodeComposite.java
+++ src/main/java/raf/dsw/gerumap/mapRepository/composite/MapNodeComposite.java
@@ -15,6 +15,7 @@ public abstract class MapNodeComposite extends MapNode{
 
     //kompozicija gde je OVO onaj crni romb a strelica je kod MapNode
     private List<MapNode> children;
+    private int numberingChildren = 1;
 
     public MapNodeComposite(String ime, MapNode parent) {
         super(ime, parent);
@@ -31,5 +32,9 @@ public abstract class MapNodeComposite extends MapNode{
 
     public void deleteChild(MapNode mapNode) {
         children.remove(mapNode);
+    }
+
+    public int getNumberingChildren() {
+        return numberingChildren++;
     }
 }
