266af641e0b3a12a38a1f5286343c92a82e70064
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-07 19:01:49 +0100

    dodat delete u meni

--- src/main/java/raf/dsw/gerumap/gui/swing/view/MyMenuBar.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/MyMenuBar.java
@@ -11,6 +11,7 @@ public class MyMenuBar extends JMenuBar {
         JMenu filemenu = new JMenu("File");//ono sto se pojavljuje gore
         filemenu.setMnemonic(KeyEvent.VK_F);//Koristi ALT i jos neki key(taster) da otvori File, u nasem slucaju ALT+F
         filemenu.add(MainFrame.getInstance().getActionManager().getNewProjectAction());
+        filemenu.add(MainFrame.getInstance().getActionManager().getDeleteChildAction());
         filemenu.add(MainFrame.getInstance().getActionManager().getInfoAction());
 
         this.add(filemenu);
