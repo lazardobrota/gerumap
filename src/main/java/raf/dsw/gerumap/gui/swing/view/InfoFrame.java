5ff98f715d888e4f019cf4ed0634c58ff9bc809b
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-11-11 21:08:55 +0100

    promenjena velicina InfoFrame

--- src/main/java/raf/dsw/gerumap/gui/swing/view/InfoFrame.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/InfoFrame.java
@@ -29,7 +29,7 @@ public class InfoFrame extends JDialog {
         Dimension screenSize = kit.getScreenSize();
         int screenHeight = (int) screenSize.getHeight();
         int screenWidth = (int) screenSize.getWidth();
-        this.setSize(screenWidth / 5 + 20, screenHeight / 2 - 50);
+        this.setSize(screenWidth / 5 + 20, screenHeight / 2 + 100);
         this.setLocationRelativeTo(MainFrame.getInstance());
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         this.setTitle("Informacije studenata");
