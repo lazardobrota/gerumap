47a22e1cdbdaf48aa60932fe70b7ca8fab5bfad1
asakotic6821rn <asakotic6821rn@raf.rs>
2022-11-14 16:50:03 +0100

    Prepravljeno da se u fajlu ispisuju svi erori

--- src/main/java/raf/dsw/gerumap/gui/swing/error/FileLogger.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/error/FileLogger.java
@@ -1,18 +1,21 @@
 package raf.dsw.gerumap.gui.swing.error;
 
+
 import raf.dsw.gerumap.core.ErrorLogger;
 import raf.dsw.gerumap.gui.swing.messageGen.Message;
 
 import java.io.*;
 
+
 public class FileLogger implements ErrorLogger {
 
     @Override
     public void log(Message message) {
+
         FileWriter fileWriter = null;
         BufferedWriter bufferedWriter = null;
         try {
-            fileWriter = new FileWriter("src\\main\\resources\\loggerFile.txt");
+            fileWriter = new FileWriter("src\\main\\resources\\loggerFile.txt",true);
             bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.append(message.toString()).append("\n");
         } catch (IOException e) {
