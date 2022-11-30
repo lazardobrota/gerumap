350e1e14a03a1609e89be6f38ce3cc4e83ee50bf
ldobrota7421rn <ldobrota7421rn@raf.rs>
2022-11-30 20:25:04 +0100

    stroke je sada int i Element je abstract

--- src/main/java/raf/dsw/gerumap/mapRepository/implementation/Element.java
+++ src/main/java/raf/dsw/gerumap/mapRepository/implementation/Element.java
@@ -4,10 +4,10 @@ import raf.dsw.gerumap.mapRepository.composite.MapNode;
 
 import java.awt.*;
 
-public class Element extends MapNode {//todo treba da bude abstract i da se promeni Factory za element
+public abstract class Element extends MapNode {//todo treba da bude abstract i da se promeni Factory za element
 
     private Color color;
-    private Stroke stroke;//debljina linije
+    private int stroke;//debljina linije
 
     public Element(String ime, MapNode parent) {
         super(ime, parent);
