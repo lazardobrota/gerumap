22262275fe478cd82af1a0d6c2647cd266191bb7
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-12-02 18:21:48 +0100

    Erase sada brise krugove

--- src/main/java/raf/dsw/gerumap/state/concrate/EraseState.java
+++ src/main/java/raf/dsw/gerumap/state/concrate/EraseState.java
@@ -1,12 +1,34 @@
 package raf.dsw.gerumap.state.concrate;
 
+import raf.dsw.gerumap.gui.swing.view.ElementPainter;
 import raf.dsw.gerumap.gui.swing.view.MindMapView;
+import raf.dsw.gerumap.mapRepository.implementation.Pojam;
 import raf.dsw.gerumap.state.State;
+
+import java.awt.*;
+import java.util.Iterator;
 
 public class EraseState extends State {
 
     @Override
     public void pressed(int x, int y, MindMapView m) {
-        System.out.println("Erase");
+
+        Pojam pojam = new Pojam(new Dimension(10, 10), new Point(x, y));
+
+
+        Iterator<ElementPainter> iterator = m.getElementPainterList().iterator();
+        while (iterator.hasNext()) {
+
+            ElementPainter elementPainter = iterator.next();
+            if (elementPainter.elementAt(pojam, pojam.getPosition())) {
+
+                elementPainter.getElement().setParent(null);//Sklanja mu roditelja
+                m.getMindMap().deleteChild(elementPainter.getElement());//Roditelju sklanja taj element
+                m.getElementPainterList().remove(elementPainter);//Listi paintera sklanja taj elementPainter
+
+                System.out.println("Erase");
+                return;
+            }
+        }
     }
 }
