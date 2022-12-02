d0ee83e40a7f5042aa7742f952f61a8719b7e786
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-12-02 15:33:29 +0100

    uradjen elementAt metoda i vise se ne uzima ovde da centar kruga bude mis

--- src/main/java/raf/dsw/gerumap/gui/swing/view/PojamPainter.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/PojamPainter.java
@@ -2,10 +2,10 @@ package raf.dsw.gerumap.gui.swing.view;
 
 import raf.dsw.gerumap.mapRepository.implementation.Element;
 import raf.dsw.gerumap.mapRepository.implementation.Pojam;
+import raf.dsw.gerumap.mapRepository.implementation.Veza;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
 
 public class PojamPainter extends ElementPainter{
 
@@ -19,17 +19,42 @@ public class PojamPainter extends ElementPainter{
         Pojam p = (Pojam) element;
         System.out.println("draw");
         g.setPaint(p.getColor());//todo treba da se doda metoda
-        
         g.setStroke(new BasicStroke(10));
-        //x - p.getDimension().width/2, y - p.getDimension().height/2 u argumentu postavljaju na sredinu misa da se postavi krug
-        shape = new Ellipse2D.Float(p.getPosition().x - (float)p.getDimension().width/2, p.getPosition().y - (float)p.getDimension().height/2,
-                p.getDimension().width, p.getDimension().height); //TODO treba elipsa sa podesenim tekstom da bude
+        int x = p.getPosition().x;
+        int y = p.getPosition().y;
+
+        shape = new Ellipse2D.Float(x, y, p.getDimension().width, p.getDimension().height); //TODO treba elipsa sa podesenim tekstom da bude
+
         g.draw(shape);
     }
 
     //todo
     @Override
     public boolean elementAt(Element element, Point position) {
+        //todo da li da vraca ze Vezu uvek false
+        if (element instanceof Veza)//Veza moze da bude na pojmu jer ih tako povezuje
+            return false;
+
+        Pojam pojam = (Pojam) this.element;//Pojam ove klase koji je vec dodat
+        Pojam that = (Pojam) element;//Neki pojam koji sada hocemo da dodamo
+
+        //Pojam
+        int x = pojam.getPosition().x;
+        int y = pojam.getPosition().y;
+        int right = x + pojam.getDimension().width;
+        int down = y + pojam.getDimension().height;
+        //That
+        int x2 = that.getPosition().x;
+        int y2 = that.getPosition().y;
+        int right2 = x2 + that.getDimension().width;
+        int down2 = y2 + that.getDimension().height;
+
+        //That zeli da se postavi na mesto gde vec postoji elipsa a to je nemoguce
+        //(x i y) ili (x i dole) se gleda da li se tu nalazi
+        if ((x <= x2 && x2 <= right) && ((y <= y2 && y2 <= down) || (y <= down2 && down2 <= down)))
+            return true;
+        if ((x <= right2 && right2 <= right) && (((y <= y2 && y2 <= down) || (y <= down2 && down2 <= down))))
+            return true;
         return false;
     }
 }
