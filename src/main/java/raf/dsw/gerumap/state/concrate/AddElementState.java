cb6a7aa1fcf72ddc706e7f7bdfdfb726326a2136
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-12-02 15:34:58 +0100

    centar kruga je mis i proverava se da li se na toj poziciji vec nalazi elipsa

--- src/main/java/raf/dsw/gerumap/state/concrate/AddElementState.java
+++ src/main/java/raf/dsw/gerumap/state/concrate/AddElementState.java
@@ -1,5 +1,6 @@
 package raf.dsw.gerumap.state.concrate;
 
+import raf.dsw.gerumap.gui.swing.view.ElementPainter;
 import raf.dsw.gerumap.gui.swing.view.MainFrame;
 import raf.dsw.gerumap.gui.swing.view.MindMapView;
 import raf.dsw.gerumap.gui.swing.view.PojamPainter;
@@ -15,12 +16,35 @@ public class AddElementState extends State {
     @Override
     public void pressed(int x, int y, MindMapView m) {
         MindMap mindMap = m.getMindMap();
-        Pojam pojam = new Pojam("Pojam" + mindMap.getNumberingChildren(), mindMap, new Dimension(50, 50), new Point(x, y));//todo na osnovu teksta napravi dimenziju
+        Dimension dimension = new Dimension(50, 50); //Lazni pojam gde samo proveravamo da li postoji nesto na tim koordinatama
+        int x2 = coordinate(x, dimension);
+        int y2 = coordinate(y, dimension);
+        Pojam pojam = new Pojam(dimension, new Point(Math.abs(x2), Math.abs(y2)));
+
+        for (ElementPainter elementPainter : m.getElementPainterList()) {
+            //Vec postoji na toj poziciji takav pojam
+            if (elementPainter.elementAt(pojam, pojam.getPosition())) {
+                return;
+            }
+        }
+
+        //Moze da se pravi pravi pojam jer ima slobodan prostor da se napravi
+        pojam = new Pojam("Pojam" + mindMap.getNumberingChildren(), mindMap, dimension, new Point(Math.abs(x2), Math.abs(y2)));//todo na osnovu teksta napravi dimenziju
         m.getElementPainterList().add(new PojamPainter(pojam));//u listu paintera za tu mapu uma se dodaje pojam
 
         //Dodaje dete i poziva se update
         mindMap.addChild(pojam);//todo treba i u stablu da se doda
         //MainFrame.getInstance().getMapTree().addChild(mindMap);
         System.out.println("Add");
+    }
+
+    private int coordinate(int c, Dimension dimension) {
+        int c2 = c - dimension.width / 2;
+
+        //Ako je van panela da ga ne postavi mnogo desno sa Math.abs
+        if (c2 < 0)
+            c2 = c;
+
+        return c2;
     }
 }
