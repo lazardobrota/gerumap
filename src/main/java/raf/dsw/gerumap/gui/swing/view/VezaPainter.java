180862cbe27b28d44f28df8f9594009143ca535a
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-12-05 22:22:55 +0100

    cuva i koordinate lokovanih delova veze za elipsu

--- src/main/java/raf/dsw/gerumap/gui/swing/view/VezaPainter.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/VezaPainter.java
@@ -15,7 +15,7 @@ public class VezaPainter extends ElementPainter{
         super(element);
     }
 
-    //todo ne treba vise puta ista veza za dva pojma
+    //todo ne treba vise puta ista veza za dva pojma i ovaj uslov za draw ne treba ovde
     @Override
     public void draw(Graphics2D g, Element element) {
         Veza veza = (Veza) element;
@@ -65,20 +65,58 @@ public class VezaPainter extends ElementPainter{
             }
         }
 
+        //Cuva i kooridante delova elipse gde se zakljuca veza
+        veza.setPocetak(new Point(xx, yy));
+        veza.setKraj(new Point(xx2, yy2));
+
         shape = new Line2D.Float(xx, yy, xx2, yy2);
         g.setPaint(veza.getColor());
         g.setStroke(new BasicStroke(veza.getStroke()));
         g.draw(shape);
     }
 
-    //todo
+    //todo crta levo desno gore dole ali koordinate elipse su gore levo
     //za brisanje pojma da li smo ga pogodili
     @Override
     public boolean elementAt(Element element, Point position) {
         if (element instanceof Veza)//Mora pojam da pogodi
             return false;
 
-        Pojam pojam = (Pojam) element;
+        //this
+        Veza veza = (Veza) this.element;
+        //A
+        int x = veza.getPocetak().x;//x1
+        int y = veza.getPocetak().y;//x1
+        //B
+        int right = veza.getKraj().x;//x2
+        int down = veza.getKraj().y;//y2
+
+        if (x > right) {
+            int t = x;
+            x = right;
+            right = t;
+        }
+
+        //that
+        Pojam that= (Pojam) element;
+        int x2 = position.x;//x
+        int y2 = position.y;//y
+
+        //Ako se x2 hitbox pojma nalazi izmedju leve i desne koordinate veze
+        if (x < x2 && x2 <= right) {//todo popravi brisanje veze
+
+            //jednacina prave: y - y1 = ((y2-y1)/(x2-x1))*(x-x1)  A(x1, y1) B(x2, y2)
+            //kod nas: y2 - y = ((down - y)/(right - x)) * (x2 - x)
+            int k = (y - down) / (x - right);
+            int result = position.y - y - k * position.x + k * x;
+            System.out.println("result: " + result);
+
+            if ( -5 <= result && result <= 5)//Moze sa 10 da promasi(kao hitbox)
+                return true;
+        }
+
+        if (x == right)
+            return true;
 
         return false;
     }
