b6e3520f0d2b2d520243b71596982ef6a623a598
Lazar Dobrota <ldobrota7421rn@raf.rs>
2022-12-21 23:16:35 +0100

    restartCommands promenjen u changeCommandManager metodu koja sa uslovima zna kako undo i redo da postavi za selektovan mindmap

--- src/main/java/raf/dsw/gerumap/mapRepository/commands/CommandManager.java
+++ src/main/java/raf/dsw/gerumap/mapRepository/commands/CommandManager.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Setter
 public class CommandManager {
 
-    //todo da li treba da se cuva redo i undo ako se promeni mapa uma na tabu
     private List<AbstractCommand> commands = new ArrayList<>();
     private int counter = 0;
 
@@ -26,20 +25,29 @@ public class CommandManager {
         this.doCommandManager();
     }
 
-    public void restartCommands() {
-        commands.clear();
-        counter = 0;
-        ApplicationFramework.getInstance().getGui().disableRedoAction();
-        ApplicationFramework.getInstance().getGui().disableUndoAction();
-
-    }
-
     public void doCommandManager(){
         if(counter < commands.size()){
             commands.get(counter++).doCommand();
             ApplicationFramework.getInstance().getGui().enableUndoAction();//Undo - unazad
         }
         if(counter == commands.size()){
+            ApplicationFramework.getInstance().getGui().disableRedoAction();//Redo - unapred
+        }
+    }
+
+    public void changeCommandManager() {
+        if(counter < commands.size()){//Moze i levo i desno da ide ako je counter manji
+            ApplicationFramework.getInstance().getGui().enableUndoAction();//Undo - unazad
+            ApplicationFramework.getInstance().getGui().enableRedoAction();//Redo - unapred
+        }
+
+        if(!commands.isEmpty()){//Ako nije prazno to znaci da moze na nesto undo da se uradi
+            ApplicationFramework.getInstance().getGui().enableUndoAction();//Undo - unazad
+        }
+        if (counter == 0) {//Ako je counter 0 ne moze vise undo da radi
+            ApplicationFramework.getInstance().getGui().disableUndoAction();//Undo - unazad
+        }
+        if(counter == commands.size()){//Ako je counter skroz desno ne moze vise redo a radi
             ApplicationFramework.getInstance().getGui().disableRedoAction();//Redo - unapred
         }
     }
